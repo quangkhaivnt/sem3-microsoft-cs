738ec2ce62a9029d2c358be1669a19c6e73c11e6
khainqd <khainqd00575@fpt.edu.vn>
2018-11-01 10:16:20 +0700

    lan 1

---
+++ PracticalPaperWFP/PracticalPaperWFP/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static PracticalPaperWFP.POJO;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PracticalPaperWFP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            collection = new ObservableCollection<RootObject>();
+            this.DataContext = this;
+        }
+        public ObservableCollection<RootObject> collection { get; set; }
+
+       
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<RootObject> data = await POJO.GetVideo();
+            for (int i = 0; i < data.Count; i++)
+            {
+                
+                collection.Add(data[i]);
+            }
+            APIGridView.ItemsSource = collection;
+        }
+    }
+}
